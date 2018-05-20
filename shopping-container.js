fb563172a0826fd5e2fb98aee412ccd8ceba0505
seufster <seufster@gmail.com>
2018-05-20 07:29:30 -0400

    init

---
+++ shopping-container.js
@@ -0,0 +1,47 @@
+/*
+* Author Edward Seufert
+*/
+'use-strict';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as appPrefActions from '../../core/common/apppref-actions';
+import fuLogger from '../../core/common/fu-logger';
+import ShoppingView from '../../memberView/shopping/shopping-view';
+
+
+class ShoppingContainer extends Component {
+	constructor(props) {
+		super(props);
+
+	}
+
+	componentDidMount() {
+		//this.props.actions.initMember();
+	}
+
+  render() {
+			fuLogger.log({level:'TRACE',loc:'ShoppingContainer::render',msg:"Hi there"});
+      return (
+				<ShoppingView/>
+			);
+  }
+}
+
+ShoppingContainer.propTypes = {
+	appPrefs: PropTypes.object,
+	lang: PropTypes.string,
+	appGlobal: PropTypes.object,
+	actions: PropTypes.object
+};
+
+function mapStateToProps(state, ownProps) {
+  return {lang:state.lang, appPrefs:state.appPrefs};
+}
+
+function mapDispatchToProps(dispatch) {
+  return { actions:bindActionCreators(appPrefActions,dispatch) };
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(ShoppingContainer);
