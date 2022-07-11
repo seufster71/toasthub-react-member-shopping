4ff629ee6dbedb2c38490b904d6d960516dfcf15
seufster71 <seufster@gmail.com>
2022-07-11 11:25:13 -0400

    cleanup

--- shopping-container.js
+++ shopping-container.js
@@ -4,19 +4,16 @@
 'use-strict';
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useNavigate, useLocation } from "react-router-dom";
 // import * as actions from '../../core/common/apppref-actions';
 import fuLogger from '../../core/common/fu-logger';
 import ShoppingView from '../../memberView/shopping/shopping-view';
 
 
-function ShoppingContainer() {
+function ShoppingContainer({location,navigate}) {
 	const itemState = useSelector((state) => state.shopping);
 	const session = useSelector((state) => state.session);
 	const appPrefs = useSelector((state) => state.appPrefs);
 	const dispatch = useDispatch();
-	const location = useLocation();
-	const navigate = useNavigate();
 
 	useEffect(() => {
 		// dispatch(actions.init());
